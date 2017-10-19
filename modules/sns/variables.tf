885d645bec468b7ef589288bff915effe4a4b66b
Caleb Groom <cgroom@rackspace.com>
2017-10-18 23:10:32 -0500

    Adds SNS module

---
+++ modules/sns/variables.tf
@@ -0,0 +1,21 @@
+variable "lambda_arn" {}
+variable "lambda_function_name" {}
+variable "sns_arn" {}
+
+variable "assume_role_policy" {
+  default = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }
+]
+}
+EOF
+}
