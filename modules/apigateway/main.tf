6637c6546b62993daf8006f4ef5632c09a71c619
Szilveszter Farkas <szilveszter.farkas@rackspace.com>
2017-11-09 10:11:44 +0100

    Some lifecycle configuration helps with the problem
    Let's use this instead of the intermediate step, seems much cleaner.

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -69,19 +69,17 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "${var.description} (stage: ${var.stage})"
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${var.stage}_deploy"
+  stage_name  = "${var.stage}"
 
   variables = {
     "version" = "${md5(data.template_file.swagger_file.rendered)}"
   }
-}
 
-resource "aws_api_gateway_stage" "stage" {
-  stage_name = "${var.stage}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  deployment_id = "${aws_api_gateway_deployment.deployment.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 data "aws_acm_certificate" "ssl_cert" {
@@ -102,7 +100,7 @@ resource "aws_api_gateway_base_path_mapping" "basepath" {
   count = "${var.enable_custom_domain}"
 
   api_id      = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${aws_api_gateway_stage.stage.stage_name}"
+  stage_name  = "${aws_api_gateway_deployment.stage.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.domain.domain_name}"
   base_path   = "${var.base_path}"
 }
