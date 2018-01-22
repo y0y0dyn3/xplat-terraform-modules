c8db701489e272b3df12578e025d311d327de644
Szilveszter Farkas <szilveszter.farkas@rackspace.com>
2018-01-22 15:03:32 +0100

    Also export the API Gateway REST API name

--- modules/apigateway/outputs.tf
+++ modules/apigateway/outputs.tf
@@ -2,6 +2,10 @@ output "api_id" {
   value = "${aws_api_gateway_rest_api.api.id}"
 }
 
+output "api_name" {
+  value = "${aws_api_gateway_rest_api.api.name}"
+}
+
 output "invoke_url" {
   # If we have a custom domain set up, we should point the endpoint to that.
   # Otherwise take the deployment's invoke URL, and strip the intermediary
