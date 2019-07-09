498fc79b5428a74cd663c36ca35638a761562ea1
Michael Humphrey <mhumphre@rackspace.com>
2019-07-09 11:48:47 -0500

    remove api_gateway_arn default value

--- modules/regional-waf/variables.tf
+++ modules/regional-waf/variables.tf
@@ -8,7 +8,7 @@ variable "region" {
 }
 variable "api_gateway_arn" {
     type = "string"
-    default = "arn:aws:apigateway:${var.region}::/restapis/${var.api_id}/stages/${var.stage}"
+    default = ""
 }
 variable "api_name" {
     type = "string"
