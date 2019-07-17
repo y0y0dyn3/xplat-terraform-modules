c702293f7738d1d7ba6d4999fca052cd0075e5c1
Michael Humphrey <mhumphre@rackspace.com>
2019-07-17 13:57:00 -0500

    remove stray variable

--- modules/regional-waf/main.tf
+++ modules/regional-waf/main.tf
@@ -375,15 +375,6 @@ resource "aws_wafregional_byte_match_set" "byte_set_webroot_requests" {
 
 # Rules
 
-variable "metric_api_name" {
-  type = "string"
-  value = "${replace("blah", "/[^a-zA-Z0-9_]/", "")}"
-}
-
-output "metric_api_name_out" {
-  value = "${var.metric_api_name}"
-}
-
 ## 10.
 ## Generic
 ## IP Blacklist
